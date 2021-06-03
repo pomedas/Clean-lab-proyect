06c7a4d3b0380a3af42e04563cac887edb8d7e6a
peregrin-tuk <eric.thalhammer@gmx.net>
2021-06-03 18:25:09 +0200

    net: color change + collision / player representation
    Net
    - net changes color based on damage
    - net changes color based on stretch
    - net has a mesh collider
    - tags for collision (island, fish, trash)
    - rip and restore methods
    
    Player & Scene
    - Boats Collide on Y > 0
    - Net Collides at > = -0.2
    - Players are now Spheres without colliders

--- Assets/scripts/Net.cs
+++ Assets/scripts/Net.cs
@@ -23,11 +23,15 @@ public class Net : MonoBehaviour
     public int trashCount;
 
     LineRenderer lineRenderer;
+    MeshCollider meshCollider;
+    Mesh mesh;
 
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+        mesh = new Mesh();
         vertexPosition = red_boat.position + ((blue_boat.position - red_boat.position) / 2);
     }
 
@@ -45,9 +49,21 @@ public class Net : MonoBehaviour
             float stretch = boat_distance / max_stretch;
             if (stretch > changeLineWidthFrom)
             {
-                float widthFactor = Mathf.Clamp(((stretch - 1) / (1-changeLineWidthFrom)) * -10, 0, 10);
+                float widthFactor = Mathf.Clamp(((stretch - 1) / (1-changeLineWidthFrom)) * -10, 0.01f, 10);
                 lineRenderer.widthMultiplier = widthFactor;
-                // TODO change color based on stretch
+
+                // change color based on stretch
+                Color lineColor = new Color(1, 1-stretch, 1-stretch, 1);
+                lineRenderer.startColor = lineColor;
+                lineRenderer.endColor = lineColor;
+            }
+            else
+            {
+                // change color based on damage
+                float colorFactor = damage / strength * changeLineWidthFrom;
+                Color lineColor = new Color(1, 1 - colorFactor, 1 - colorFactor, 1);
+                lineRenderer.startColor = lineColor;
+                lineRenderer.endColor = lineColor;
             }
             
 
@@ -57,7 +73,12 @@ public class Net : MonoBehaviour
             float speed = Mathf.Lerp(0, max_speed, Mathf.Clamp01(Vector3.Distance(vertexPosition, boatsMidPoint) / slow_down_distance));
             vertexPosition += direction * Time.deltaTime * speed;
 
+            // draw line
             DrawQuadraticBezierCurve(red_boat.position, vertexPosition, blue_boat.position);
+
+            // update collider
+            lineRenderer.BakeMesh(mesh, true);
+            meshCollider.sharedMesh = mesh;
         }
     }
 
@@ -74,12 +95,61 @@ public class Net : MonoBehaviour
         }
     }
 
-    // TODO: fish and trash collider + collect methods
-    // TODO: island collider + rip
-    // TODO: change texture and color based on damage
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision with " + collision.gameObject.name);
 
-    void Rip()
+        if (collision.gameObject.name == "Harbour")
+        {
+            Debug.Log("We're in the Harbour");
+        }
+        if (collision.gameObject.tag == "Island")
+        { 
+            Debug.Log("Crashed into Island");
+            Rip();
+        }
+        if (collision.gameObject.tag == "Fish")
+        {
+            Debug.Log("Collected Fish");
+            CollectFish(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Trash")
+        {
+            Debug.Log("Collected Fish");
+            CollectTrash(collision.gameObject);
+        }
+    }
+
+    void CollectFish(GameObject fish)
+    {
+        fishCount++;
+        // TODO play sound effect, remove collected gameobject from scene
+        // NTH increase "weight" of the net (by making the line thicker in the middle and speed slower
+    }
+
+    void CollectTrash(GameObject trash)
+    {
+        trashCount++;
+        damage++;
+        // TODO play sound effect, remove collected gameobject from scene
+        // NTH increase "weight" of the net (by making the line thicker in the middle and speed slower
+    }
+
+    public void Rip()
     {
         ripped = true;
+        mesh.Clear();
+        meshCollider.sharedMesh = null;
+        meshCollider.enabled = false;
+        lineRenderer.enabled = false;
     }
+
+    public void Restore()
+    {
+        damage = 0;
+        ripped = false;
+        meshCollider.enabled = true;
+        lineRenderer.enabled = true;
+    }
+
 }
