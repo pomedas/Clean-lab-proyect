ec9ca133de3867087fa7d9af1bc9cf9c133dcdf2
WS119175\fubintlab <pedro.omedas@upf.edu>
2022-04-14 17:26:55 +0200

    Updated to work with new tracking in the lab

--- Assets/scripts/utilities/utility tracking for unity/Uniducial/FiducialController.cs
+++ Assets/scripts/utilities/utility tracking for unity/Uniducial/FiducialController.cs
@@ -25,10 +25,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.Net.Sockets;
+using System.Net;
 
 public class FiducialController : MonoBehaviour
 {
+    public float keysVel = 0.05f;
     public int MarkerID = 0;
+
+    public PlayerDetection playerDetection;
+    private float[] playerDetection_pos;
 
     public enum RotationAxis { Forward, Back, Up, Down, Left, Right };
 
@@ -46,6 +52,7 @@ public class FiducialController : MonoBehaviour
     public float CameraOffset = 10;
     public RotationAxis RotateAround = RotationAxis.Back;
     private UniducialLibrary.TuioManager m_TuioManager;
+    private UdpClient client;
     private Camera m_MainCamera;
 
     //members
@@ -61,20 +68,54 @@ public class FiducialController : MonoBehaviour
     private bool m_IsVisible;
 
     public float RotationMultiplier = 1;
+    private Socket client_s;
+
 
     List<Vector3> places;
 
+
+    private float[] SendAndReceive(float[] dataOut)
+    {
+        //initialize socket
+        float[] floatsReceived;
+        client_s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        client_s.Connect(System.Net.IPAddress.Parse("127.0.0.1"), 4444);
+        if (!client_s.Connected)
+        {
+            Debug.LogError("Connection Failed");
+            return null;
+        }
+
+        //convert floats to bytes, send to port
+        var byteArray = new byte[dataOut.Length * 4];
+        Buffer.BlockCopy(dataOut, 0, byteArray, 0, byteArray.Length);
+        client_s.Send(byteArray);
+
+        //allocate and receive bytes
+        byte[] bytes = new byte[4000];
+        int idxUsedBytes = client_s.Receive(bytes);
+        //print(idxUsedBytes + " new bytes received.");
+
+        //convert bytes to floats
+        floatsReceived = new float[idxUsedBytes / 4];
+        Buffer.BlockCopy(bytes, 0, floatsReceived, 0, idxUsedBytes);
+
+        client_s.Close();
+        return floatsReceived;
+    }
     void Awake()
     {
         places = new List<Vector3>();
         this.m_TuioManager = UniducialLibrary.TuioManager.Instance;
 
         //uncomment next line to set port explicitly (default is 3333)
-        //tuioManager.TuioPort = 7777;
+        this.m_TuioManager.TuioPort = 7777;
 
         this.m_TuioManager.Connect();
 
-
+        this.client = new UdpClient();
+        //IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3333); // endpoint where server is listening
+        //client.Connect(ep);
 
         //check if the game object needs to be transformed in normalized 2d space
         if (isAttachedToGUIComponent())
@@ -105,10 +146,48 @@ public class FiducialController : MonoBehaviour
         {
             Debug.LogError("There is no main camera defined in your scene.");
         }
+
+        playerDetection = FindObjectOfType<PlayerDetection>();
+        //    Debug.Log(playerDetection.positions[0]);
     }
 
     void Update()
     {
+        //Debug.Log(playerDetection.positions[0]);
+        /*  if (this.m_TuioManager.IsConnected){
+              Debug.Log("Tuio connected");
+          }else{
+              Debug.Log("tuio not connected");
+          }*/
+        /*  if (this.m_TuioManager.IsMarkerAlive(this.MarkerID)){
+              Debug.Log("Marker found");
+          }else{
+              Debug.Log("marker not found");
+          }   */
+
+        //var remoteEP = new IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 3333);
+        //var data = this.client.Receive(ref remoteEP);
+        //float[] data_in;
+        float[] floatArray = { (float)this.MarkerID };
+
+        // data_in = SendAndReceive(floatArray);
+
+        playerDetection_pos = new float[] { playerDetection.positions[2 * MarkerID], playerDetection.positions[2 * MarkerID + 1] };
+
+        this.m_ScreenPosition.x = playerDetection_pos[0];
+        this.m_ScreenPosition.y = playerDetection_pos[1];
+
+        //Debug.Log("Player " + MarkerID.ToString() + " x: " + playerDetection_pos[0].ToString());
+
+        UpdateTransform();
+        //Debug.Log(data_in[0].ToString());
+        /*if(MarkerID == (int)data_in[0]){
+             this.m_ScreenPosition.x = data_in[1];
+             this.m_ScreenPosition.y = data_in[2];
+             UpdateTransform();
+         }*/
+
+
         if (this.m_TuioManager.IsConnected
             && this.m_TuioManager.IsMarkerAlive(this.MarkerID))
         {
@@ -147,78 +226,81 @@ public class FiducialController : MonoBehaviour
                 //keyboard commands to test
                 if (MarkerID == 0)
                 {
+                    // Debug.Log(transform.position);
+
                     if (Input.GetKey(KeyCode.UpArrow))
                     {
-                        transform.position += Vector3.forward*0.5f;
+                        transform.position += Vector3.forward * keysVel;
                     }
                     if (Input.GetKey(KeyCode.DownArrow))
                     {
-                        transform.position += Vector3.forward * -0.5f;
-                    } 
+                        transform.position += Vector3.forward * -keysVel;
+                    }
                     if (Input.GetKey(KeyCode.RightArrow))
                     {
-                        transform.position += Vector3.right * 0.5f;
+                        transform.position += Vector3.right * keysVel;
                     }
                     if (Input.GetKey(KeyCode.LeftArrow))
                     {
-                        transform.position += Vector3.right * -0.5f;
-                    } 
+                        transform.position += Vector3.right * -keysVel;
+                    }
                 }
+
                 if (MarkerID == 1)
                 {
                     if (Input.GetKey(KeyCode.Keypad8))
                     {
-                        transform.position += Vector3.forward * 0.5f;
+                        transform.position += Vector3.forward * keysVel;
                     }
                     if (Input.GetKey(KeyCode.Keypad2))
                     {
-                        transform.position += Vector3.forward * -0.5f;
+                        transform.position += Vector3.forward * -keysVel;
                     }
                     if (Input.GetKey(KeyCode.Keypad6))
                     {
-                        transform.position += Vector3.right * 0.5f;
+                        transform.position += Vector3.right * keysVel;
                     }
                     if (Input.GetKey(KeyCode.Keypad4))
                     {
-                        transform.position += Vector3.right * -0.5f;
+                        transform.position += Vector3.right * -keysVel;
                     }
                 }
                 if (MarkerID == 2)
                 {
                     if (Input.GetKey(KeyCode.W))
                     {
-                        transform.position += Vector3.forward * 0.5f;
+                        transform.position += Vector3.forward * keysVel;
                     }
                     if (Input.GetKey(KeyCode.S))
                     {
-                        transform.position += Vector3.forward * -0.5f;
+                        transform.position += Vector3.forward * -keysVel;
                     }
                     if (Input.GetKey(KeyCode.D))
                     {
-                        transform.position += Vector3.right * 0.5f;
+                        transform.position += Vector3.right * keysVel;
                     }
                     if (Input.GetKey(KeyCode.A))
                     {
-                        transform.position += Vector3.right * -0.5f;
+                        transform.position += Vector3.right * -keysVel;
                     }
                 }
                 if (MarkerID == 3)
                 {
                     if (Input.GetKey(KeyCode.I))
                     {
-                        transform.position += Vector3.forward * 0.5f;
+                        transform.position += Vector3.forward * keysVel;
                     }
                     if (Input.GetKey(KeyCode.K))
                     {
-                        transform.position += Vector3.forward * -0.5f;
+                        transform.position += Vector3.forward * -keysVel;
                     }
                     if (Input.GetKey(KeyCode.L))
                     {
-                        transform.position += Vector3.right * 0.5f;
+                        transform.position += Vector3.right * keysVel;
                     }
                     if (Input.GetKey(KeyCode.J))
                     {
-                        transform.position += Vector3.right * -0.5f;
+                        transform.position += Vector3.right * -keysVel;
                     }
                 }
             }
@@ -241,11 +323,15 @@ public class FiducialController : MonoBehaviour
         //position mapping
         if (this.IsPositionMapped)
         {
+            
             //calculate world position with respect to camera view direction
             float xPos = this.m_ScreenPosition.x;
             float yPos = this.m_ScreenPosition.y;
+
             if (this.InvertX) xPos = 1 - xPos;
             if (this.InvertY) yPos = 1 - yPos;
+
+            // Debug.Log("test");
 
             if (this.m_ControlsGUIElement)
             {
@@ -253,6 +339,7 @@ public class FiducialController : MonoBehaviour
             }
             else
             {
+                Debug.Log("here");
                 this.m_WorldPosition = new Vector3(xPos, 0f, yPos) * 100;
                 /*if (places.Count > 3)
                 {
@@ -263,10 +350,10 @@ public class FiducialController : MonoBehaviour
                 */
 
                 transform.position = this.m_WorldPosition;
-                Logger.AddUserPosition(gameObject.name, m_WorldPosition);
+                //Logger.AddUserPosition(gameObject.name, m_WorldPosition);
             }
         }
-        
+
 
         //rotation mapping
         if (this.IsRotationMapped)
@@ -298,9 +385,11 @@ public class FiducialController : MonoBehaviour
         }
     }
 
-    private Vector3 computemedian() {
+    private Vector3 computemedian()
+    {
         Vector3 newpos = Vector3.zero;
-        foreach (Vector3 v in places) {
+        foreach (Vector3 v in places)
+        {
             newpos += v;
         }
         newpos = newpos / places.Count;
