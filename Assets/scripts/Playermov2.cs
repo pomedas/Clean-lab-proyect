5f69b636327ab75b1cc3c2fd583a0960966a3f36
MartaAlet <72511288+MartaAlet@users.noreply.github.com>
2022-05-20 23:31:42 +0200

    Added Timer, Transparent Barnyards, Fixed Battle with Fox, Added gameplay with golden chicken

--- Assets/scripts/Playermov2.cs
+++ Assets/scripts/Playermov2.cs
@@ -7,12 +7,16 @@ public class Playermov2 : MonoBehaviour
     public GameObject itsBarnyard;
     public GameObject oponentBarnyard;
     public GameObject circle_chicken;
+    public GameObject circle_golden_chicken;
+    private GameObject circle;
     private GameObject chicken;
     public GameObject circle_fox;
     public GameObject circle_battle_fox;
     private GameObject fox;
+    public GameObject timer;
     private bool chicken_caught;
     private bool fox_caught;
+    private bool battling_fox;
 
     private float itsBarnyard_min_x;
     private float itsBarnyard_max_x;
@@ -22,14 +26,17 @@ public class Playermov2 : MonoBehaviour
     private float oponentBarnyard_max_z;
     private float oponentBarnyard_min_x;
     private float oponentBarnyard_max_x;
+    private float time_fox_caught;
     // Start is called before the first frame update
     void Start()
     {
         circle_chicken.SetActive(false);
+        circle_golden_chicken.SetActive(false);
         chicken_caught = false;
         circle_fox.SetActive(false);
         circle_battle_fox.SetActive(false);
         fox_caught = false;
+        battling_fox = false;
         itsBarnyard_min_x = itsBarnyard.transform.position.x - itsBarnyard.GetComponent<Barnyard>().xDepth;
         itsBarnyard_max_x = itsBarnyard.transform.position.x + itsBarnyard.GetComponent<Barnyard>().xDepth;
         itsBarnyard_min_z = itsBarnyard.transform.position.z - itsBarnyard.GetComponent<Barnyard>().zDepth;
@@ -49,7 +56,7 @@ public class Playermov2 : MonoBehaviour
             if (chicken_caught == true)
             {
                 chicken.SetActive(true);
-                circle_chicken.SetActive(false);
+                circle.SetActive(false);
                 Vector3 randomPosition = new Vector3(Random.Range(itsBarnyard_min_x, itsBarnyard_max_x), 0.5f, Random.Range(itsBarnyard_min_z, itsBarnyard_max_z));
                 chicken.transform.position = randomPosition;
                 chicken_caught = false;
@@ -61,21 +68,35 @@ public class Playermov2 : MonoBehaviour
         {
             fox.SetActive(true);
             circle_fox.SetActive(false);
-            Vector3 randomPosition = new Vector3(Random.Range(oponentBarnyard_min_x, oponentBarnyard_max_x), 0.5f, Random.Range(oponentBarnyard_min_z, oponentBarnyard_max_z));
+            Vector3 randomPosition = new Vector3(Random.Range(oponentBarnyard_min_x +5, oponentBarnyard_max_x - 5), 0.5f, Random.Range(oponentBarnyard_min_z, oponentBarnyard_max_z));
             fox.transform.position = randomPosition;
             fox_caught = false;
+        }
+
+        if ((time_fox_caught - 5 > timer.GetComponent<Timer>().timeRemaining) && (battling_fox))
+        {
+            circle_battle_fox.SetActive(false);
+            battling_fox = false;
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if ((transform.position.x < itsBarnyard_min_x-5) && (transform.position.x > oponentBarnyard_max_x+5))
         {
-            if ((other.gameObject.CompareTag("Chicken"))&& (chicken_caught == false) && (fox_caught == false))
+            if ((((other.gameObject.CompareTag("Chicken")) || (other.gameObject.CompareTag("GoldenChicken"))) && ((chicken_caught == false) && (fox_caught == false)))&&(battling_fox==false))
             {
                 Debug.Log("collisionnnn with chicken");
                 chicken = other.gameObject;
                 other.gameObject.SetActive(false);
-                circle_chicken.SetActive(true);
+                if (other.gameObject.CompareTag("Chicken"))
+                {
+                    circle = circle_chicken;
+                }
+                else
+                {
+                    circle = circle_golden_chicken;
+                }
+                circle.SetActive(true);
                 chicken_caught = true;
             }
             if ((other.gameObject.CompareTag("Fox"))&& (chicken_caught == false) && (fox_caught == false))
@@ -94,6 +115,8 @@ public class Playermov2 : MonoBehaviour
                 Debug.Log("Battling fox");
                 other.gameObject.SetActive(false);
                 circle_battle_fox.SetActive(true);
+                battling_fox = true;
+                time_fox_caught = timer.GetComponent<Timer>().timeRemaining;
             }
         }
     }
