cbf617c499d5d6f0dd9696ae4621a0e21992e56e
jordialfonsop <jordialfonsop@gmail.com>
2022-05-29 13:41:13 +0200

    Created main menu

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance; // 1
+
+    [SerializeField] public MenuP1 P1;
+    [SerializeField] public MenuP2 P2;
+    float a;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+    void Update()
+    {
+        P1 = P1.GetComponent<MenuP1>();
+        P2 = P2.GetComponent<MenuP2>();
+        if (P1.is1Inside == true && P2.is2Inside == true){
+            SceneManager.LoadScene("IntSysTemplate");
+        }
+    }
+
+}
